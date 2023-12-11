252e80e81f58999de0e1d985e520f559fea69d00
Jitty Tresa Thomas <jittytresa@gmail.com>
2023-12-11 18:39:49 +0530

    feat:initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# WorldMapGame
+
+This branch would only consist of html and css for the game
---
+++ script/game.js
@@ -0,0 +1,12 @@
+let questions = new Map([
+    [1, "Which continent is India part of?"],
+    [2, "Which is the largest continent in area?"],
+    [3, "Which is the smallest continent in area?"],
+    [4, "Which continent is also called as an 'Island-Continent'?"],
+    [5, "The largest mountain in the world, Mount Everest is situated in which continent?"],
+    [6, "On Which Continent Is Egypt?"],
+    [7, "On which continent can you find France?"],
+    [8, "Which is the Second biggest continent of the world?"],
+    [9, "Columbus found which continent?"],
+    [10, "Where is Amazon River?"]
+]);
